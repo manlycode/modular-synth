ee0879a3ae5b7d901d5b180dfd34e598161664a9
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2020-01-28 22:05:39 -0500

    Use 8 pins to display beat.

--- sequencer/src/main.cpp
+++ sequencer/src/main.cpp
@@ -12,15 +12,31 @@ byte count;
 #define COUNT_PIN0 PIN2
 #define COUNT_PIN1 PIN3
 #define COUNT_PIN2 PIN4
+#define COUNT_PIN3 PIN5
+#define COUNT_PIN4 PIN6
+#define COUNT_PIN5 PIN7
+#define COUNT_PIN6 PIN8
+#define COUNT_PIN7 PIN9
 
 void setup() {
   count = 0;
   pinMode(COUNT_PIN0, OUTPUT);
   pinMode(COUNT_PIN1, OUTPUT);
   pinMode(COUNT_PIN2, OUTPUT);
-  digitalWrite(COUNT_PIN0, LOW);
+  pinMode(COUNT_PIN3, OUTPUT);
+  pinMode(COUNT_PIN4, OUTPUT);
+  pinMode(COUNT_PIN5, OUTPUT);
+  pinMode(COUNT_PIN6, OUTPUT);
+  pinMode(COUNT_PIN7, OUTPUT);
+
+  digitalWrite(COUNT_PIN0, HIGH);
   digitalWrite(COUNT_PIN1, LOW);
   digitalWrite(COUNT_PIN2, LOW);
+  digitalWrite(COUNT_PIN3, LOW);
+  digitalWrite(COUNT_PIN4, LOW);
+  digitalWrite(COUNT_PIN5, LOW);
+  digitalWrite(COUNT_PIN6, LOW);
+  digitalWrite(COUNT_PIN7, LOW);
 
   pinMode(LED_PIN, OUTPUT);
   // put your setup code here, to run once:
