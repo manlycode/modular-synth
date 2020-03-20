90f876801bbfdea48f61bd89093c2ac4a047eb4e
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2020-03-20 11:24:48 -0400

    Add encoder test.

--- arduino-vco/src/main.cpp
+++ arduino-vco/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <pins.h>
+#include <Encoder.h>
+
+Encoder myEnc(ENCA, ENCB);
+
+long oldPosition  = -999;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Basic Encoder Test:");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
 }
