c733f79f61b96c7c5bafd79f599dce334c5762b0
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2020-05-18 18:24:29 -0400

    Save point for vco.

--- arduino-vco-dac/include/pins.h
+++ arduino-vco-dac/include/pins.h
@@ -1,18 +1,17 @@
 // Encoder Pins
-#define ENCA 3
-#define ENCB 2
+#define ENCA 2
+#define ENCB 3
 
 // Green LED Pins
-#define GRN_LED1 10
-#define GRN_LED2 11
-#define GRN_LED3 8
-#define GRN_LED4 7
+#define GRN_LED1 5
+#define GRN_LED2 6
+#define GRN_LED3 7
+#define GRN_LED4 8
 
 // Red LED Pins
-#define RED_LED1 5
-#define RED_LED2 6
+#define RED_LED1 9
+#define RED_LED2 12
 
 // IO Pins
 #define BUTTON_PIN 4
-#define INPUT_PIN A3
-#define OUTPUT_PIN 9
+#define INPUT_PIN A0
--- arduino-vco-dac/src/main.cpp
+++ arduino-vco-dac/src/main.cpp
@@ -1,31 +1,121 @@
+#include <Arduino.h>
+#include <pins.h>
+#include <Encoder.h>
+
 #include <MozziGuts.h>
 #include <Oscil.h> // oscillator template
-#include <tables/sin2048_int8.h> // sine table for oscillator
+// #include <tables/sin2048_int8.h> // sine table for oscillator
+#include <tables/sin1024_int8.h>
+#include <tables/triangle2048_int8.h>
+#include <tables/saw2048_int8.h> // sine table for oscillator
+#include <tables/square_no_alias_2048_int8.h> // sine table for oscillator
+#include <mozzi_fixmath.h>
+#include <frequency_table.h>
 
-// use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file of table #included above
-Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
+const uint8_t greenPins[] = { GRN_LED1, GRN_LED2, GRN_LED3, GRN_LED4 };
+const uint8_t redPins[] = { RED_LED1, RED_LED2 };
 
-// control variable, use the smallest data size you can for anything used in audio
+
+Encoder myEnc(ENCA, ENCB);
+
+byte encoderCounter = 0;
 byte gain = 16;
 int offset = 113;
+long oldPosition  = -999;
+long newPosition = oldPosition;
+
+float frequency = 0.0f;
+uint16_t frequencyIdx = 0;
+
+uint8_t oldMode = 0;
+uint8_t modeIdx = 0;
 
 
-void setup(){
-  startMozzi(); // start with default control rate of 64
-  aSin.setFreq(3300.0f); // set the frequency
+uint8_t currentPin;
+ 
+// Mozzi Variables
+Oscil <SIN1024_NUM_CELLS, AUDIO_RATE> aSin(SIN1024_DATA);
+Oscil <TRIANGLE2048_NUM_CELLS, AUDIO_RATE> aTri(TRIANGLE2048_DATA);
+Oscil <SAW2048_NUM_CELLS, AUDIO_RATE> aSaw(SAW2048_DATA);
+Oscil <SQUARE_NO_ALIAS_2048_NUM_CELLS, AUDIO_RATE> aSquare(SQUARE_NO_ALIAS_2048_DATA);
+
+#define CONTROL_RATE 64 // Hz, powers of 2 are most reliable
+
+void setup() {
+  // Serial.begin(9600);
+  delay(200);
+
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(INPUT_PIN, INPUT);
+
+  // Setup Green LEDs
+  for (uint8_t i = 0; i < 4; i++) {
+    currentPin = greenPins[i];
+    pinMode(currentPin, OUTPUT);
+  }
+
+  digitalWrite(greenPins[modeIdx], HIGH);
+
+  // Setup Red LEDs
+  for (uint8_t i = 0; i < 2; i++) {
+    currentPin = redPins[i];
+    pinMode(currentPin, OUTPUT);
+  }
+
+  // Mozzi setup
+  startMozzi(CONTROL_RATE); // :)
+  aSin.setFreq(frequency);
 }
-
 
 void updateControl(){
-  // as byte, this will automatically roll around to 255 when it passes 0
+  // if (encoderCounter % 32 == 0) {
+  //   // newPosition = myEnc.read();
+  //   if (newPosition != oldPosition) {
+  //     oldPosition = newPosition;
+  //     modeIdx = (newPosition / 4) % 4; 
+  //     if (oldMode != modeIdx) {
+  //       // digitalWrite(greenPins[oldMode], LOW);
+  //       // digitalWrite(greenPins[modeIdx], HIGH);
+  //       oldMode = modeIdx;
+  //     }
+  //   }
+  // }
+
+  // encoderCounter++;
+
+  // put changing controls in here
+  frequencyIdx = mozziAnalogRead(INPUT_PIN);
+  frequency = pgm_read_float_near(FREQUENCY_TABLE_DATA + frequencyIdx);
+  aSin.setFreq(frequency);
+  // aTri.setFreq(frequency);
+  // aSaw.setFreq(frequency);
+  // aSquare.setFreq(frequency);
 }
 
 
-int updateAudio(){
-  return ((aSin.next()+offset)*gain);
+int updateAudio() {
+  switch (modeIdx) {
+  case 0:
+    return ((aSin.next()+offset)*gain);
+    break;
+  case 1:
+    return ((aTri.next()+offset)*gain);
+    break;
+  case 2:
+    return ((aSaw.next()+offset)*gain);
+    break;
+  case 3:
+    return ((aSquare.next()+offset)*gain);
+    break;
+  
+  default:
+    return ((aSin.next()+offset)*gain);
+    break;
+  }
 }
 
 
-void loop(){
-  audioHook(); // required here
+void loop() {
+  // Serial.println(digitalRead(BUTTON_PIN));
+  audioHook(); // required here  
 }
